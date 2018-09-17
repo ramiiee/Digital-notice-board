4879eef13c0e52d08e319d4f51044d153481af8b
ramiiee <42035191+ramiiee@users.noreply.github.com>
2018-09-17 13:30:23 -0400

    Update TemporaryRepoistory.java

--- src/main/java/com/proj/nb/board/repository/TemporaryRepoistory.java
+++ src/main/java/com/proj/nb/board/repository/TemporaryRepoistory.java
@@ -12,12 +12,9 @@ public interface TemporaryRepoistory extends JpaRepository<TemporaryAdmin , Long
 	TemporaryAdmin findById(Long id);
 	
 	TemporaryAdmin findByName(String name);
-//	TemporaryAdmin fingByImname(String temporaryimagename);
+	
+	
 
-//	void delete(String imagename);
-	
-	
-//chala chaala jagrathga spellings rayali ledha darunamaina errors vatayi...nenu delte anir asa delete badhulu motham thappani vachesindhi madda -_-
 	void deleteByName(String name);
 
 	void deleteAllByName(String name);
