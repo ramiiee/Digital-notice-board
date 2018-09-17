34f9c7c09b5bc81368f8809f42f15343c28af490
ramiiee <42035191+ramiiee@users.noreply.github.com>
2018-09-17 13:31:14 -0400

    Update PermanentRepository.java

--- src/main/java/com/proj/nb/board/repository/PermanentRepository.java
+++ src/main/java/com/proj/nb/board/repository/PermanentRepository.java
@@ -11,11 +11,10 @@ import com.proj.nb.board.model.PermanentImages;
 
 @Transactional
 public interface PermanentRepository extends JpaRepository<PermanentImages , Long>{
-		//Login findByUsername(String username);
+		
 		
 		PermanentImages findById(Integer id);
-	//	PermanentImages findByImname(String imagename);
-		//findByImageName ani isthe error vastadi findByImagename...adhe ila isthe crct...'camelCase' lo isthe automatic ga spring own implementation tho manakem kavalo adhistadi 
+			
 		PermanentImages findByImagename(String imagename);
 		
 		List<PermanentImages> findAllByCategory(Integer category);
